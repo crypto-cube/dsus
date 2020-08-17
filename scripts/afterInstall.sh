a6cb76036c55c9d9c2352f4378648ce059e5a288
int3hh <octav@cryptocube.ro>
2020-08-17 20:50:05 +0300

    mk certs on install

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -6,4 +6,11 @@ else
     useradd -m dsus
 fi
 
+if [ ! -f "/etc/dsus/certs/publickey.pub" ]; then
+    echo "Generating keys ..."
+    openssl genrsa -out /etc/dsus/certs/server.key 2048
+    openssl rsa -in /etc/dsus/certs/server.key -pubout > /etc/dsus/certs/publickey.pub
+    openssl req -new -x509 -sha256 -key /etc/dsus/certs/server.key -out /etc/dsus/certs/server.crt -days 5475 -subj "/C=RO/ST=Bucharest/L=Bucharest/O=CryptoCube SRL/OU=CORE/CN=dsus.it"
+fi
+
 chmod 755 -R /etc/dsus/ && echo "Permissions set!"
